20b0eaa0525d030f78e84ee97e1208a0378bdff5
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2023-09-05 10:28:52 -0300

    first commit

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -2,7 +2,20 @@ const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
-function bash() {}
+function bash() {
+   process.stdout.write('prompt > ')
+  process.stdin.on('data',(data)=>{
+   const input = data.toString().trim();
+    const args = input.split(' ');
+    const cmd = args.shift(); 
+   cmd in commands?commands[cmd](print, args.join(' ')):print(`command not found: ${cmd}`)
+
+  })
+}
+
+const print = (output)=>{
+   process.stdout.write(output)
+process.stdout.write("\nprompt > ")}
 
 bash();
 module.exports = {
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -2,20 +2,91 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd() {}
+function pwd(print) {
+    print(process.cwd())
+}
 
-function date() {}
+function date(print) {
+    print(Date())
+}
 
-function echo() {}
+function echo(print , args) {
+    print(args);
+    
+}
 
-function ls() {}
+function ls(print) {
+    fs.readdir('.',(error,files)=>{
+        if(error){
+            throw new Error("Mensaje de error"); 
+        }
+        else{
+            const filesAsString = files.join("")
+            print(filesAsString)}
 
-function cat() {}
+    
+    })
+   
+}
 
-function head() {}
+function cat(print,args) {
+    fs.readFile(args,'utf-8',(error,data)=>{
+     if(error){throw new Error("Mensaje de error")}else{print(data)}
+    })
 
-function tail() {}
+}
 
-function curl() {}
+function head(print,args) {
+    fs.readFile(args,'utf-8',(error,data)=>{
+        if(error){
+            throw new Error("Mensaje de error")
+        }
+        else{ 
+            
+            const codeLines = data.split('\n')
+            const firstLine = codeLines.slice(0,1);
 
-module.exports = {};
+            print(firstLine.join('\n'))
+
+        }
+    })
+}
+
+function tail(print,args) {
+    fs.readFile(args, 'utf-8', (error,data)=>{
+        if(error){
+            throw new Error("Mensaje de error")
+        }
+        else{ 
+            const lastLines = data.split('\n').at(-1).trim()
+
+            print(lastLines)
+
+        }
+    })
+}
+
+function curl(print,args) {
+    utils.request(args, (error,response)=>{
+        if(error){
+            throw new Error("Mensaje de error")
+        }
+        else{ 
+            print(response)
+        
+        }
+    })
+ 
+    
+
+}
+module.exports = {
+    pwd,
+    date,
+    echo,
+    ls,
+    cat,
+    head,
+    tail,
+    curl
+};
