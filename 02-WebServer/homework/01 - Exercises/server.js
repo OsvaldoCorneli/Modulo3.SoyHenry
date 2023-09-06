bf188d220c3dc3b50e89fa069f072f873521d6f7
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2023-09-06 13:16:27 -0300

    homework 01 exercises

--- 02-WebServer/homework/01 - Exercises/server.js
+++ 02-WebServer/homework/01 - Exercises/server.js
@@ -2,9 +2,43 @@ var fs = require("fs");
 var http = require("http");
 /* ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️ */
 /* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
-const PORT = null;
+const PORT = 3001;
+
+
+
 
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
-module.exports =
-  /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
+module.exports = http
+.createServer((request ,response)=>{
+  console.log(`Server raised in port ${PORT}`) ;
+if(request.url === "/api"){
+  fs.readFile("./utils/dogsData.json", (err,data)=>{
+      if(err){
+      response.writeHead(404,{"Content-Type": "text/plain"})
+      response.end("json not found")}
+      else{response.writeHead(200,{"Content-Type": "application/json"})
+      response.end(data)}
+
+  })
+  return
+ } 
+
+ if(request.url === "/allDogs"){
+  fs.readFile("./utils/allDogs.html","utf8",(err,data)=>{
+    if(err){
+      response.writeHead(404,{"Content-Type": "text/plain"})
+     response.end("json not found")}
+      else{response.writeHead(200,{"Content-Type": "text/html"})
+     response.end(data)}
+  })
+  return
+ }
+
+ response.writeHead(404,{"Content-Type": "text/plain"})
+ return response.end("Route not found")
+
+
+}).listen(PORT, "localhost")
+  /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR 
+  DE NULL POR EL SERVIDOR */
   null;
